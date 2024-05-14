df59b116a8cde3181cd97a859852536026ffd900
riyalohia <riyalh1997@gmail.com>
2024-05-14 11:30:00 +0530

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import LoadMore from "../components/LoadMore";
 import { data } from "./_data";
 
 async function Home() {
+  const data = fetchAnime()
   return (
     <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
       <h2 className="text-3xl text-white font-bold">Explore Anime</h2>
